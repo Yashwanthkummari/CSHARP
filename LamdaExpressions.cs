c99d37e15d4e84bed83f5b4897018226b61f2b52
“Enter <yashwanth.k1119@gmail.com>
2023-07-09 22:42:42 +0530

    [Yashwanth]add: Lamdaexpressions

---
+++ LamdaExpressions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    internal class LamdaExpressions
+    
+        {
+            static void main(string[] args)
+            {
+                
+                List<int> numbers = new List<int>() {1, 3, 12, 15, 9, 18, 36, 47, 9, 31};
+
+                Console.Write("The list : ");
+                foreach (var value in numbers)
+                {
+                    Console.Write("{0} ", value);
+                }
+                Console.WriteLine();
+
+                var square = numbers.Select(x => x * x);
+    
+                Console.Write("Squares : ");
+                foreach (var value in square)
+                {
+                    Console.Write("{0} ", value);
+                }
+                Console.WriteLine();
+
+                List<int> divBy3 = numbers.FindAll(x => (x % 3) == 0);
+            
+                Console.Write("Numbers Divisible by 3 : ");
+                foreach (var value in divBy3)
+                {
+                    Console.Write("{0} ", value);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+
---
+++ regex.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace project
+{
+    internal class regex
+     {
+        static void main(string[] args)
+        {
+            string[] inputname = { "yashwanth", "Yashth", "YahWtH", "K.Yashwanth", "k.Yaaashhhwaaanasgbnhb" };
+            string[] inputnumber = {"9123982301","123456789", "9845721598","321-1234-5678", "09876543210"};
+            string[] inputemail = { "yashwant.k1119@gmail.com", "Yashwwan123jmahsd22@gmail.com",
+                             "3@gmail.com"," aaabbbwwwhh33355443644535sssnasdfgfsdf@gmail.com,K.yashwanth@yahoo.com"};
+           foreach (string nu in inputnumber)
+            {
+                Console.WriteLine("{0} {1} a valid mobile number.", nu,
+                            isValidMobileNumber(nu) ? "is" : "is not");
+            }
+
+            Console.ReadKey();
+            foreach(string na in inputname)
+            {
+                Console.WriteLine(" {0} a Valid name ",na, isvalidname(na) ? "is" : "is not");
+            }
+            Console.ReadKey();
+            foreach (string e in inputemail)
+            {
+                Console.WriteLine(" {0} a Valid email ",e, isvalidemail(e) ? "is" : "is not");
+
+            }
+            Console.ReadKey();
+        }   
+        public static bool isValidMobileNumber(string inputMobileNumber)
+        {
+          string iPnumberRegex = @"^[6-9]{1}[0-9]{9}$";
+
+              Regex numberregex = new Regex(iPnumberRegex);
+
+            if (numberregex.IsMatch(inputMobileNumber))
+                return (true);
+            else
+                return (false);
+        }
+        public static bool isvalidname(string inputname)
+        {
+            string iPnameregex = @"(^[a-z]{}$)|(^[a-zA-z]$)| (^[A-Za-z]{1}.[a-zA-Z]{1}[a-z]{8}$)
+                                  (^\s$)";
+            Regex nameregex = new Regex(iPnameregex);
+          if (nameregex.IsMatch(inputname)) 
+                
+                return (true);
+            else
+
+                return (false);
+        }
+        public static bool isvalidemail(string inputemail)
+        {
+            string iPemailregex = @"(([a-z){1,20}.[a-z]{1}@[a-z]{5}.(com|in|jhgv))|([a-z]@[a-z].[a-z]{3})|
+                                    \s[a-zA-Z0-9]?@[a-z].(com|yahoo|.in))";
+           
+            Regex emailregex = new Regex(iPemailregex);
+
+            if (emailregex.IsMatch(inputemail))
+
+                return (true);
+            else
+                return (false);
+        }
+        }
+    }
+
+
+
